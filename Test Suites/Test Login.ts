de12d2bae61888f4f5c4b9b0bc97bfe2d0a0524f
NapiProject <97792882+NapiProject@users.noreply.github.com>
2022-02-13 20:31:25 +0700

    Add Read Data from excel File

---
+++ Test Suites/Test Login.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6839575f-bba7-459a-9d93-0a5398867c25</testSuiteGuid>
+   <testCaseLink>
+      <guid>67fdd2c6-9948-4abb-a6d9-5beaa40c8d10</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/Login/Skenario Login/0001 - Valid Data</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e61a05a8-d9a5-4341-bd4e-dd29964f0b33</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/Login/Skenario Login/0002 - Invalid Data</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c1849eba-0fbe-451f-ad00-85934f9ce6ec</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/Login/Skenario Login/0003 - Username Null</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>13c42906-37a4-4283-b189-f6343eb3153e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/Login/Skenario Login/0004 - Password Null</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
