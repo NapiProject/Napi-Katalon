4c42c836aff0093028a87c0a2a6150e74403b523
NapiProject <97792882+NapiProject@users.noreply.github.com>
2022-07-31 00:26:28 +0700

    Add function read data from txt file

---
+++ Keywords/keywordUI/fileTxt.groovy
@@ -0,0 +1,36 @@
+package keywordUI
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.configuration.RunConfiguration
+import org.apache.commons.io.FileUtils
+
+import internal.GlobalVariable
+
+public class fileTxt {
+	@Keyword
+	public static def getDataFromFileTxt () {
+		String folder = RunConfiguration.getProjectDir()
+		//path folder file
+		File folderPath = new File(folder + '/Plugins/NapiProject.txt')
+		//untuk membaca isi file
+		String fileContent = FileUtils.readFileToString(folderPath)
+		return fileContent.toString()
+	}
+}
